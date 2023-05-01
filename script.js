61aac1899fbb128dd64b2ac2c60264c719d5241d
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-05-01 20:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-//your code here
-let inputData=document.getElementById('evaluatedText');
-let count=document.getElementById('letterCount');
-inputData.addEventListener('input',()=>{
-    let element = inputData.value;
-    let n=element.length;
-    count.innerText=n;
-})
-count.innerText=n;
+function countChars(obj) {
+  document.getElementById("letterCount").innerHTML = obj.value.length;
+}
